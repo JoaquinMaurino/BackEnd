042a4317cec398118ea9b4854d0ff9c3d9452b02
Joaquin <joaquin9918@gmail.com>
2022-10-27 15:28:15 -0300

    Desafio 1

--- clase2/MauriñoJoaquin.js
+++ clase2/MauriñoJoaquin.js
@@ -1,25 +1,33 @@
 class Usuario{
-    constructor(nombre, apellido, libros, mascotas){
+    constructor(nombre, apellido ){
 this.nombre = nombre;
 this.apellido = apellido;
-this.libros = libros;
-this.mascotas = mascotas
+this.libros = [];
+this.mascotas = []
     }
     getFullName(){
-
+        return this.nombre +" "+ this.apellido
     }
-    addMascota(){
-
+    addMascota(nombre){
+this.mascotas.push(nombre);
     }
     countMascotas(){
-
+return this.mascotas.length
     }
-    addBook(){
-
+    addBook(nombre, autor){
+this.libros.push({nombre, autor})
     }
     getBookNames(){
-
+return this.libros.map(libro => libro.nombre)
     }
 }
 
-const usuario = new Usuario()
+let usuario1 = new Usuario("Joaquin", "Mauriño");
+
+usuario1.addMascota("Coco", "Bruno", "Nina", "Oliver")
+usuario1.addBook("El señor de las moscas","William Golding")
+usuario1.addBook("Fundacion", "Isaac Asimov");
+
+console.log(usuario1.getFullName());
+console.log(usuario1.countMascotas());
+console.log(usuario1.getBookNames());
